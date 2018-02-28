13ab8b049f883fa3f492a6cdb66b4d1b8831a248
yel <yel@sucsoft.com>
2018-02-28 16:43:42 +0800

    1.3.5

---
+++ index.js
@@ -0,0 +1,7 @@
+import 'angular'
+import 'angular-ui-router';
+import 'angular-sanitize';
+import 'angular-animate';
+import './js/ionic.min';
+import './js/ionic-angular.min';
+export var ionic = 'ionic';
